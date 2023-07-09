cdaf58a2a730ce7c352cfd29d0426b96dd077053
bruceabrigo <bruceabrigo@outlook.com>
2023-07-09 12:43:11 -0400

    Init commit

---
+++ src/components/shared/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './navbar.scss'
+
+const Navbar = () => (
+    <>
+        <header>
+            <h1>AppTrack</h1>
+        </header>
+    </>
+)
+
+export default Navbar
