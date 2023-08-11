5c549d004461e6c2beac5895713bb436f06e25ea
bruceabrigo <bruceabrigo@outlook.com>
2023-08-10 20:59:56 -0500

    added heading for applications

--- src/components/home.js
+++ src/components/home.js
@@ -10,9 +10,18 @@ const Home = (props) => {
         New Application
     </button>
     <div className="centered-content">
-      <div className="homebox">
+      <div className="content-container">
         <div className="content-bar">
             <h1 className="content-bar-title">Applications</h1>
+        </div>
+        <div className="application-container">
+            <ul className="applications">
+                <li>Company</li>
+                <li>Role</li>
+                <li>Submitted</li>
+                <li>Interview</li>
+                <li>Status</li>
+            </ul>
         </div>
       </div>
     </div>
