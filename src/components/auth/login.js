bcb8b88f16af84365f0ee7db2064324b0dc08bd2
bruceabrigo <bruceabrigo@outlook.com>
2023-08-24 14:05:19 -0500

    Installed Bootsrap and Added email form to SignIn page

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { signIn } from "../../api/auth";
 import './login.scss'
 
+import Form from 'react-bootstrap/Form'
+
 const LogIn = (props) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
@@ -36,6 +38,19 @@ const LogIn = (props) => {
             <h1 className="greeting">Nice to See You Again!</h1>
             <div className="login">
                 <h1>Welcome Back!</h1>
+                    <Form onSubmit={onLogIn}>
+                        <Form.Group controlId='email'>
+                            <Form.Label>Your Email</Form.Label>
+                            <Form.Control
+                                required
+                                type='email'
+                                name='email'
+                                value={email}
+                                placeholder='Your Email'
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Form>
             </div>
         </div>
     )
